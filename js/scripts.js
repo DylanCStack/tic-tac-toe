4ab4c1513da9d57af416cda5b9453825112c27fc
Dylan Stackhouse and Jennifer Beem <jennifer@>
2017-01-26 10:03:44 -0800

    retrieve player names and display them

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 //business logic
-
-
+function player(name, symbol){//add tally for victories
+  this.name = name;
+  this.symbol = symbol;
+}
 
 
 
 
 // user-interface logic
 $(document).ready(function() {
+  $("form#names").submit(function(event) {
+    event.preventDefault();
+    var inputtedName1 = $("input[name=name1]").val();
+    var inputtedName2 = $("input[name=name2]").val();
+
+    $("#player1").text(inputtedName1);
+    $("#player2").text(inputtedName2);
+
+    $("#current-player").text(inputtedName1);
+
   $(".cell").click(function() {
     $(this).children("p").text("X");
+  })
   })
 })
